2b1676a24dd99e6927ef3bbbbb61b6ae7178edb5
superxc <superxcgm@gmail.com>
2018-03-30 15:24:16 +0800

    finish section1/PracticeB

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,11 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        PracticeA practiceA = new PracticeA();
+        return practiceA.collectSameElements(collection1, collection2.get(0));
     }
 }
