cb750e0793937c81a032e2570d153596cdfb93cb
superxc <superxcgm@gmail.com>
2018-03-30 16:06:52 +0800

    finish section3/PracticeB

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -1,12 +1,25 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        List<String> collectionB = object.get("value");
+        List<String> collectionTmp = new ArrayList<>();
+        for (String s : collectionB) {
+            Integer value;
+            if ((value = collectionA.get(s)) != null) {
+                int times = value / 3;
+                while (times-- > 0) {
+                    collectionTmp.add(s);
+                }
+            }
+        }
+        Map<String , List<String >> mp = new HashMap<>();
+        mp.put("value", collectionTmp);
+        return (new PracticeA()).createUpdatedCollection(collectionA, mp);
     }
 }
